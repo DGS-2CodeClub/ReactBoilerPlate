cf965da1481f91d451d1e9dedaddf9c2c45795b7
nik <nichoals.kurtz31415@gmail.com>
2018-04-05 09:07:52 -0700

    Fixes previous bug and creates the instance in a card using material UI.

--- main/Interests.js
+++ main/Interests.js
@@ -1,84 +1,66 @@
-/**
- * Created by Nicholas on 3/14/2018.
- */
-import * as React from 'react';
+import React from 'react';
+import Card from "../node_modules/material-ui/Card/Card";
 
 class Interests extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {skillValue: '3', interestValue: '3', skillName: ''};
-
-    this.handleChangeSkill = this.handleChangeSkill.bind(this);
-    this.handleChangeInterest = this.handleChangeInterest.bind(this);
-    this.handleChangeSkillName = this.handleChangeSkillName.bind(this)
-  }
-
-  handleChangeSkill(event) {
-    this.setState({skillValue: event.target.value});
-  }
-
-  handleChangeInterest(event) {
-    this.setState({interestValue: event.target.value});
-  }
-
-  handleChangeSkillName(event) {
-    this.setState({skillName: event.target.value});
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
   }
 
   render() {
-    this.state.skillName = 'What do you do?'
-
     return (
-      <div className = 'main'>
-        <div className = 'top_buffer' />
-        <div className = 'title'>
-          WHAT VOODOO DO YOU DO?
-        </div>
-        <ul className = 'interest_input'>
-          <li>
-            <input
-              className = 'skill_name_input'
-              type = 'text'
-              onChange = {this.handleChangeSkillName}
-              value = {this.state.skillName}
-              autofocus
-            />
+      <div>
+        <Card className = 'card_main'>
+          <div className = 'title'>
+            WHAT VOODOO DO YOU DO?
+          </div>
+          <ul className = 'interest_input'>
+            <li>
+              <input
+                className = 'skill_name_input'
+                type = 'text'
+                onChange = {this.handleChangeSkillName}
+                value = {this.state.skillName}
+                autoFocus
+              />
 
-          </li>
-          <li>
-            <input
-              className = 'big_slider'
-              type = 'range'
-              min = '1'
-              max = '5'
-              step = '1'
-              onChange = {this.handleChangeSkill}
-              value = {this.state.skillValue}
-            />
-          </li>
-          <li>
-            <input
-              className = 'big_slider'
-              type = 'range'
-              min = '1'
-              max = '5'
-              step = '1'
-              onChange = {this.handleChangeInterest}
-              value = {this.state.interestValue}
-            />
+            </li>
+            <li>
+              <input
+                className = 'big_slider'
+                type = 'range'
+                min = '1'
+                max = '5'
+                step = '1'
+                onChange = {this.handleChangeSkill}
+                value = {this.state.skillValue}
+              />
+            </li>
+            <li>
+              <input
+                className = 'big_slider'
+                type = 'range'
+                min = '1'
+                max = '5'
+                step = '1'
+                onChange = {this.handleChangeInterest}
+                value = {this.state.interestValue}
+              />
 
-          </li>
-        </ul>
-        <div className = 'footer'>
-          <input
-            className = 'submit_button'
-            type = 'submit'
-            value = 'Submit'
-          />
-        </div>
+            </li>
+          </ul>
+          <div className = 'footer'>
+            <input
+              className = 'submit_button'
+              type = 'submit'
+              value = 'Submit'
+            />
+          </div>
+        </Card>
       </div>
-    )
+    );
   }
 }
-
-export default Interests
+export default Interests;
--- main/Main.js
+++ main/Main.js
@@ -1,14 +1,13 @@
 /**
  * Created by Nicholas on 3/14/2018.
  */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Interests from './Interests';
+import Interest from './Interests';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(<Interests/>),
+    React.createElement(Interest),
     document.getElementById('mount')
   );
 });
